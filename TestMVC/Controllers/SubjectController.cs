7496a5236d7b73bd525ce959c45e14121ff7c4b4
Ly111999 <39553327+Ly111999@users.noreply.github.com>
2018-12-04 10:00:02 +0700

    create and list

---
+++ TestMVC/Controllers/SubjectController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestMVC.Context;
+using TestMVC.Models;
+
+namespace TestMVC.Controllers
+{
+    public class SubjectController : Controller
+    {
+        private SubjectContext db = new SubjectContext();
+        // GET: Subject
+        public ActionResult Index()
+        {
+            return View(db.Subjects.ToList());
+        }
+
+     
+        // GET: Subject/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Subject/Create
+        [HttpPost]
+        public ActionResult Create(Subject subject)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Subjects.Add(subject);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                return View(subject);
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+     
+    }
+}
